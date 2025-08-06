6f05a8a389f6732872712c269cf84586d93a5fa0
Ali Mansoor <alimansoorqadirghumro@gmail.com>
2025-08-06 20:08:32 +0400

    Updated functionalities

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
-  };
+      let total = 0;
+
+      cart.forEach((item) => {
+        const cost = parseFloat(item.cost.substring(1));
+        const quantity = item.quantity;
+
+        total += quantity * cost;
+      })
+       return total; 
+      };
 
   const handleContinueShopping = (e) => {
-   
+    alert('Functionallity to be added for future reference');
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }))
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+        const unitPrice = parseFloat(item.cost.substring(1));
+        const quantity = item.quantity;
+        const total = unitPrice * quantity;
+        return total;
   };
 
   return (
@@ -61,8 +73,8 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
     </div>
   );
-};
 
+}
 export default CartItem;
 
 
